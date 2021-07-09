ebae8f37b48dd8ed131bd7476cfaaffaba509a04
Yaniv <yhou@apricot.one>
2021-07-09 17:02:02 +0800

    simplify interface

--- rust/src/consts.rs
+++ rust/src/consts.rs
@@ -28,6 +28,8 @@ pub mod price_summaries {
     declare_id!("G1cmF3D5PAEAjnwdMFbcGQbnBmWNH7t4hv8cpmfHzS2V");
 }
 
+
+// commands
 pub const CMD_ADD_USER_AND_DEPOSIT: u8 = 0x10;
 pub const CMD_DEPOSIT: u8 = 0x11;
 pub const CMD_WITHDRAW: u8 = 0x12;
@@ -39,3 +41,132 @@ pub const CMD_UPDATE_USER_CONFIG: u8 = 0x17;
 pub const CMD_MARGIN_SWAP: u8 = 0x18;
 pub const CMD_UPDATE_USER_ASSET_CONFIG: u8 = 0x19;
 pub const CMD_WITHDRAW_AND_REMOVE_USER: u8 = 0x1a;
+
+
+// errors
+pub const ERR_INCORRECT_BASE_PDA:u32 = 0x1000;
+pub const ERR_INCORRECT_USER_PAGES_STATS:u32 = 0x1001;
+pub const ERR_INCORRECT_USERS_PAGE:u32 = 0x1002;
+pub const ERR_INCORRECT_USER_INFO:u32 = 0x1003;
+pub const ERR_INCORRECT_ASSET_POOL:u32 = 0x1004;
+pub const ERR_INCORRECT_ASSET_PRICE:u32 = 0x1005;
+pub const ERR_INCORRECT_ASSET_POOL_SPL:u32 = 0x1006;
+pub const ERR_INCORRECT_USER_ASSET_INFO:u32 = 0x1007;
+pub const ERR_MISSING_ACTIVE_ACCOUNTS:u32 = 0x1008;
+pub const ERR_INCORRECT_INTERMEDIATE_SPL:u32 = 0x1009;
+pub const ERR_INCORRECT_SELL_MARKET:u32 = 0x100a;
+pub const ERR_INCORRECT_BUY_MARKET:u32 = 0x100b;
+pub const ERR_INCORRECT_SERUM_PROGRAM:u32 = 0x100c;
+pub const ERR_INCORRECT_ADMIN:u32 = 0x100d;
+pub const ERR_INCORRECT_INTERMEDIATE_SPL_OWNER:u32 = 0x100e;
+pub const ERR_INCORRECT_POOL_LIST:u32 = 0x100f;
+pub const ERR_INCORRECT_POOL_SUMMARIES:u32 = 0x1010;
+pub const ERR_INCORRECT_PRICE_SUMMARIES:u32 = 0x1011;
+pub const ERR_INCORRECT_PRICE_PDA:u32 = 0x1012;
+
+// errors about data
+pub const ERR_MISSING_PAGE_ID:u32 = 0x2000;
+pub const ERR_PAGE_ID_TOO_LARGE:u32 = 0x2001;
+pub const ERR_MISSING_AMOUNT:u32 = 0x2002;
+pub const ERR_MISSING_MINT_SEED_STR:u32 = 0x2003;
+pub const ERR_MISSING_ACTIVE_MINT_SEED_STR:u32 = 0x2004;
+pub const ERR_WRONG_DATA_SIZE:u32 = 0x2005;
+
+
+// internal logic error
+pub const ERR_ACCOUNT_ALREADY_ADDED:u32 = 0x3000;
+pub const ERR_NO_AVAILABLE_SLOTS:u32 = 0x3001;
+pub const ERR_ACCOUNT_NOT_ADDED:u32 = 0x3002;
+pub const ERR_WALLET_DID_NOT_SIGN:u32 = 0x3003;
+pub const ERR_MAXIMUM_NUM_POOLS_REACHED:u32 = 0x3004;
+pub const ERR_USER_HAS_NO_SUCH_ASSET:u32 = 0x3005;
+pub const ERR_NEED_AT_LEAST_BUY_OR_SELL:u32 = 0x3006;
+pub const ERR_INSUFFICIENT_FEES:u32 = 0x3007;
+
+// user logic error
+pub const ERR_DEPOSIT_LESS_THAN_MINIMUM:u32 = 0x4000;
+pub const ERR_INSUFFICIENT_DEPOSIT:u32 = 0x4001;
+pub const ERR_POOL_NO_FREE_FUND:u32 = 0x4002;
+pub const ERR_PLEASE_WITHDRAW_ALL:u32 = 0x4003;
+pub const ERR_INSUFFICIENT_BORROW_POWER:u32 = 0x4004;
+pub const ERR_CANNOT_REPAY_MORE_THAN_DEBT:u32 = 0x4005;
+pub const ERR_WITHDRAWL_BELOW_MIN_COLLATERAL_RATIO:u32 = 0x4006;
+pub const ERR_LIQUIDATION_NOT_REACHED:u32 = 0x4007;
+pub const ERR_LIQUIDATOR_ASKED_TOO_MUCH_COLLATERAL:u32 = 0x4008;
+pub const ERR_ACCOUNT_NOT_ENOUGH_DEBT_FOR_LIQUIDATION:u32 = 0x4009;
+pub const ERR_ACCOUNT_NOT_ENOUGH_COLLATERAL_FOR_LIQUIDATION:u32 = 0x400a;
+pub const ERR_EXCEEDS_LIQUIDATION_LIMIT:u32 = 0x400b;
+pub const ERR_SELF_LIQUIDATION_THRESHOLD_TOO_SMALL:u32 = 0x400c;
+pub const ERR_POST_SELF_LIQUIDATION_TARGET_TOO_SMALL:u32 = 0x400d;
+pub const ERR_POST_EXTERN_LIQUIDATION_TARGET_TOO_SMALL:u32 = 0x400e;
+pub const ERR_SELF_LIQUIDATION_NOT_REACHED:u32 = 0x400f;
+pub const ERR_SELF_LIQUIDATION_TARGET_EXCEEDED:u32 = 0x4010;
+pub const ERR_SELF_LIQUIDATION_HIGH_SLIPPAGE:u32 = 0x4011;
+pub const ERR_MAX_NUM_ASSETS_REACHED:u32 = 0x4012;
+pub const ERR_SWAP_BOUGHT_LESS_THAN_MIN:u32 = 0x4013;
+pub const ERR_ASSET_NOT_USED_AS_COLLATERAL:u32 = 0x4014;
+
+
+pub const USER_INFO_SEED: &'static str = "UserInfo";
+pub const USER_PAGES_STATS_SEED: &'static str = "UserPagesStats";
+
+
+
+#[inline(always)]
+pub fn bytes_to_str(key_bytes:&[u8]) -> &str {
+    unsafe { std::mem::transmute(key_bytes) }
+}
+
+#[inline(always)]
+pub fn pool_id_to_seed_str<'a>(pool_id: u8, buffer: &'a mut [u8;8]) -> &'a str {
+    *buffer = *b"POOL____";
+    buffer[6] = (pool_id / 16) + b'a';
+    buffer[7] = (pool_id % 16) + b'a';
+    bytes_to_str(buffer)
+}
+
+#[inline(always)]
+pub fn get_base_pda() -> Pubkey {
+    base_pda::ID
+}
+
+#[inline(always)]
+pub fn get_pool_summaries_k() -> Pubkey  {
+    pool_summaries::ID
+}
+
+#[inline(always)]
+pub fn get_price_summaries_k() -> Pubkey  {
+    price_summaries::ID
+}
+
+#[inline(always)]
+pub fn get_user_pages_stats_k() -> Pubkey {
+    Pubkey::create_with_seed(&base_pda::ID, USER_PAGES_STATS_SEED, &program::ID).unwrap()
+}
+
+#[inline(always)]
+pub fn get_users_page_k(page_id: usize) -> Pubkey {
+    let seed = format!("UsersPage_{}", page_id);
+    Pubkey::create_with_seed(&base_pda::ID, &seed, &program::ID).unwrap()
+}
+
+#[inline(always)]
+pub fn get_asset_pool_k(pool_id: u8) -> Pubkey {
+    let mut mint_seed_buffer = [0 as u8; 8];
+    let pool_seed_str = pool_id_to_seed_str(pool_id, &mut mint_seed_buffer);
+    Pubkey::create_with_seed(&base_pda::ID, pool_seed_str, &program::ID).unwrap()
+}
+
+#[inline(always)]
+pub fn get_asset_pool_spl_k(token_program_id: &Pubkey, pool_id: u8) -> Pubkey {
+    let mut mint_seed_buffer = [0 as u8; 8];
+    let pool_seed_str = pool_id_to_seed_str(pool_id, &mut mint_seed_buffer);
+    Pubkey::create_with_seed(&base_pda::ID, pool_seed_str, token_program_id).unwrap()
+}
+
+#[inline(always)]
+pub fn get_user_info_k(user_wallet_key: &Pubkey) -> Pubkey {
+    Pubkey::create_with_seed(user_wallet_key, USER_INFO_SEED, &program::ID).unwrap()
+}
+
--- rust/src/instructions.rs
+++ rust/src/instructions.rs
@@ -2,6 +2,7 @@ use solana_program::{
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
 };
+use spl_token;
 
 use crate::consts;
 
@@ -77,14 +78,34 @@ pub fn mut_cast<T>(data: &mut [u8] ) -> &mut T {
 }
 
 pub fn deposit(
-    user_wallet: &Pubkey,
-    user_spl: &Pubkey,
-    user_info: &Pubkey,
-    asset_pool: &Pubkey,
-    asset_pool_spl: &Pubkey,
-    pool_summaries: &Pubkey,
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    amount: u64,
+    pool_id: u8,
+) -> Instruction {
+    deposit_full(
+        user_wallet,
+        user_spl,
+        &consts::get_user_info_k(user_wallet),
+        &consts::get_asset_pool_k(pool_id),
+        &consts::get_asset_pool_spl_k(&spl_token::ID, pool_id),
+        &consts::get_pool_summaries_k(),
+        &spl_token::ID,
+        &consts::program::ID,
+        amount,
+        pool_id,
+    )
+}
+
+pub fn deposit_full(
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    user_info: &Pubkey,         // consts::get_user_info_k(user_wallet_key)
+    asset_pool: &Pubkey,        // consts::get_asset_pool_k(pool_id)
+    asset_pool_spl: &Pubkey,    // consts::get_asset_pool_spl_k(token_program, pool_id)
+    pool_summaries: &Pubkey,    // consts::get_pool_summaries_k()
     token_program: &Pubkey,
-    program_id: &Pubkey,
+    program_id: &Pubkey,        // consts::program::ID
 
     amount: u64,
     pool_id: u8,
@@ -114,16 +135,40 @@ pub fn deposit(
 }
 
 pub fn withdraw(
-    user_wallet: &Pubkey,
-    user_spl: &Pubkey,
-    user_info: &Pubkey,
-    asset_pool: &Pubkey,
-    asset_pool_spl: &Pubkey,
-    pool_summaries: &Pubkey,
-    price_summaries: &Pubkey,
-    base_pda: &Pubkey,
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    withdraw_all: bool,
+    amount: u64,
+    pool_id: u8,
+) -> Instruction {
+    withdraw_full(
+        user_wallet,
+        user_spl,
+        &consts::get_user_info_k(user_wallet),
+        &consts::get_asset_pool_k(pool_id),
+        &consts::get_asset_pool_spl_k(&spl_token::ID, pool_id),
+        &consts::get_pool_summaries_k(),
+        &consts::get_price_summaries_k(),
+        &consts::get_base_pda(),
+        &spl_token::ID,
+        &consts::program::ID,
+        withdraw_all,
+        amount,
+        pool_id,
+    )
+}
+
+pub fn withdraw_full(
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    user_info: &Pubkey,         // consts::get_user_info_k(user_wallet_key)
+    asset_pool: &Pubkey,        // consts::get_asset_pool_k(pool_id)
+    asset_pool_spl: &Pubkey,    // consts::get_asset_pool_spl_k(token_program, pool_id)
+    pool_summaries: &Pubkey,    // consts::get_pool_summaries_k()
+    price_summaries: &Pubkey,   // consts::get_price_summaries_k()
+    base_pda: &Pubkey,          // consts::get_base_pda()
     token_program: &Pubkey,
-    program_id: &Pubkey,
+    program_id: &Pubkey,        // consts::program::ID
 
     withdraw_all: bool,
     amount: u64,
@@ -156,65 +201,39 @@ pub fn withdraw(
     }
 }
 
-/// withdraw, and optionally remove user
-pub fn withdraw_and_remove_user(
-    user_wallet: &Pubkey,
-    user_spl: &Pubkey,
-    user_pages_stats: &Pubkey,  // UserPagesStats, fixed account
-    users_page: &Pubkey,        // UsersPage
-    user_info: &Pubkey,
-    asset_pool: &Pubkey,
-    asset_pool_spl: &Pubkey,
-    pool_summaries: &Pubkey,
-    price_summaries: &Pubkey,
-    base_pda: &Pubkey,
-    token_program: &Pubkey,
-    program_id: &Pubkey,
-
-    withdraw_all: bool,
+pub fn borrow(
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
     amount: u64,
     pool_id: u8,
-
 ) -> Instruction {
-    let data_size = 1 + std::mem::size_of::<WithdrawParam>();
-    let mut buffer = vec![0; data_size];
-
-    buffer[0] = consts::CMD_WITHDRAW_AND_REMOVE_USER;
-    let mut param = mut_cast::<WithdrawParam>(&mut buffer[1..]);
-    param.withdraw_all = if withdraw_all {1} else {0};
-    param.amount = amount;
-    param.pool_id = pool_id;
-
-    Instruction{
-        program_id: *program_id,
-        accounts: vec![
-            AccountMeta::new_readonly(*user_wallet, true),
-            AccountMeta::new(*user_spl, false),
-            AccountMeta::new(*user_pages_stats, false),
-            AccountMeta::new(*users_page, false),
-            AccountMeta::new(*user_info, false),
-            AccountMeta::new(*asset_pool, false),
-            AccountMeta::new(*asset_pool_spl, false),
-            AccountMeta::new(*pool_summaries, false),
-            AccountMeta::new_readonly(*price_summaries, false),
-            AccountMeta::new_readonly(*base_pda, false),
-            AccountMeta::new_readonly(*token_program, false),
-        ],
-        data: buffer,
-    }
+    borrow_full(
+        user_wallet,
+        user_spl,
+        &consts::get_user_info_k(user_wallet),
+        &consts::get_asset_pool_k(pool_id),
+        &consts::get_asset_pool_spl_k(&spl_token::ID, pool_id),
+        &consts::get_pool_summaries_k(),
+        &consts::get_price_summaries_k(),
+        &consts::get_base_pda(),
+        &spl_token::ID,
+        &consts::program::ID,
+        amount,
+        pool_id,
+    )
 }
 
-pub fn borrow(
-    user_wallet: &Pubkey,
-    user_spl: &Pubkey,
-    user_info: &Pubkey,
-    asset_pool: &Pubkey,
-    asset_pool_spl: &Pubkey,
-    pool_summaries: &Pubkey,
-    price_summaries: &Pubkey,
-    base_pda: &Pubkey,
+pub fn borrow_full(
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    user_info: &Pubkey,         // consts::get_user_info_k(user_wallet_key)
+    asset_pool: &Pubkey,        // consts::get_asset_pool_k(pool_id)
+    asset_pool_spl: &Pubkey,    // consts::get_asset_pool_spl_k(token_program, pool_id)
+    pool_summaries: &Pubkey,    // consts::get_pool_summaries_k()
+    price_summaries: &Pubkey,   // consts::get_price_summaries_k()
+    base_pda: &Pubkey,          // consts::get_base_pda()
     token_program: &Pubkey,
-    program_id: &Pubkey,
+    program_id: &Pubkey,        // consts::program::ID
 
     amount: u64,
     pool_id: u8,
@@ -246,14 +265,36 @@ pub fn borrow(
 }
 
 pub fn repay(
-    user_wallet: &Pubkey,
-    user_spl: &Pubkey,
-    user_info: &Pubkey,
-    asset_pool: &Pubkey,
-    asset_pool_spl: &Pubkey,
-    pool_summaries: &Pubkey,
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    repay_all: bool,
+    amount: u64,
+    pool_id: u8,
+) -> Instruction {
+    repay_full(
+        user_wallet,
+        user_spl,
+        &consts::get_user_info_k(user_wallet),
+        &consts::get_asset_pool_k(pool_id),
+        &consts::get_asset_pool_spl_k(&spl_token::ID, pool_id),
+        &consts::get_pool_summaries_k(),
+        &spl_token::ID,
+        &consts::program::ID,
+        repay_all,
+        amount,
+        pool_id,
+    )
+}
+
+pub fn repay_full(
+    user_wallet: &Pubkey,       // user wallet account, needs to be signer
+    user_spl: &Pubkey,          // user's SPL token account
+    user_info: &Pubkey,         // consts::get_user_info_k(user_wallet_key)
+    asset_pool: &Pubkey,        // consts::get_asset_pool_k(pool_id)
+    asset_pool_spl: &Pubkey,    // consts::get_asset_pool_spl_k(token_program, pool_id)
+    pool_summaries: &Pubkey,    // consts::get_pool_summaries_k()
     token_program: &Pubkey,
-    program_id: &Pubkey,
+    program_id: &Pubkey,        // consts::program::ID
 
     repay_all: bool,
     amount: u64,
@@ -284,26 +325,63 @@ pub fn repay(
     }
 }
 
-// liquidator will help repay "borrowed" and in exchange receive "collateral" asset at 1% discount
 pub fn extern_liquidate(
-    liquidated_wallet: &Pubkey,
-    liquidator_wallet: &Pubkey,
+    liquidated_wallet: &Pubkey,         // wallet key for account to be liquidated
+    liquidator_wallet: &Pubkey,         // wallet key for liquidator, signer
+    liquidator_collateral_spl: &Pubkey, // liquidator's SPL token account for collateral asset
+    liquidator_borrowed_spl: &Pubkey,   // liquidator's SPL token account for repaid asset
+    repaid_borrow_amount: u64,
+    min_collateral_amount: u64,
+    borrowed_pool_id: u8,
+    collateral_pool_id: u8,
+) -> Instruction {
+    extern_liquidate_full(
+        liquidated_wallet,
+        liquidator_wallet,
 
-    user_info: &Pubkey, // UserInfo for liquidated_wallet
-    base_pda: &Pubkey,
+        &consts::get_user_info_k(liquidated_wallet),
+        &consts::get_base_pda(),
 
-    liquidator_collateral_spl: &Pubkey,
-    liquidator_borrowed_spl: &Pubkey,
+        liquidator_collateral_spl,
+        liquidator_borrowed_spl,
 
-    collateral_asset_pool: &Pubkey,
-    collateral_asset_pool_spl: &Pubkey,
+        &consts::get_asset_pool_k(collateral_pool_id),
+        &consts::get_asset_pool_spl_k(&spl_token::ID, collateral_pool_id),
 
-    borrowed_asset_pool: &Pubkey,
-    borrowed_asset_pool_spl: &Pubkey,
+        &consts::get_asset_pool_k(borrowed_pool_id),
+        &consts::get_asset_pool_spl_k(&spl_token::ID, borrowed_pool_id),
 
-    pool_summaries: &Pubkey,
+        &consts::get_pool_summaries_k(),
+        &spl_token::ID,
+        &consts::program::ID,
+
+        repaid_borrow_amount,
+        min_collateral_amount,
+        borrowed_pool_id,
+        collateral_pool_id,
+    )
+}
+
+// liquidator will help repay "borrowed" and in exchange receive "collateral" asset at 1% discount
+pub fn extern_liquidate_full(
+    liquidated_wallet: &Pubkey,         // wallet key for account to be liquidated
+    liquidator_wallet: &Pubkey,         // wallet key for liquidator, signer
+
+    user_info: &Pubkey,                 // consts::get_user_info_k(liquidated_wallet)
+    base_pda: &Pubkey,                  // consts::get_base_pda()
+
+    liquidator_collateral_spl: &Pubkey, // liquidator's SPL token account for collateral asset
+    liquidator_borrowed_spl: &Pubkey,   // liquidator's SPL token account for repaid asset
+
+    collateral_asset_pool: &Pubkey,     // consts::get_asset_pool_k(collateral_pool_id)
+    collateral_asset_pool_spl: &Pubkey, // consts::get_asset_pool_spl_k(token_program, collateral_pool_id)
+
+    borrowed_asset_pool: &Pubkey,       // consts::get_asset_pool_k(borrowed_pool_id)
+    borrowed_asset_pool_spl: &Pubkey,   // consts::get_asset_pool_spl_k(token_program, borrowed_pool_id)
+
+    pool_summaries: &Pubkey,            // consts::get_pool_summaries_k()
     token_program: &Pubkey,
-    program_id: &Pubkey,
+    program_id: &Pubkey,                // consts::program::ID
 
     repaid_borrow_amount: u64,
     min_collateral_amount: u64,
