4dc3004d17ff99a5690fd37648a400bddfa93a96
Yaniv <yhou@apricot.one>
2022-01-21 17:13:36 +0800

    DEV-58: add debug log tag
    bump to 0.16.12

--- sdk-ts/src/utils/Logger.ts
+++ sdk-ts/src/utils/Logger.ts
@@ -1,12 +1,21 @@
+export const DEBUG_LOG_ENV = 'APRICOT_LOG_DEBUG';
 export const INFO_LOG_ENV = 'APRICOT_LOG_INFO';
+export const TAG_DEBUG = 'DEBUG:';
 export const TAG_INFO = 'INFO:';
 export const TAG_WARN = 'WARN:';
 export const TAG_ALERT = 'ALERT:';
 export const TAG_EMERGENCY = 'EMERGENCY:';
 
+export function LogDebug(content: string) {
+  // info logging is silenced by default. Requires the APRICOT_LOG_INFO env to be set to actually log
+  if (process.env[DEBUG_LOG_ENV] === 'true') {
+    console.log(TAG_INFO + content);
+  }
+}
+
 export function LogInfo(content: string) {
   // info logging is silenced by default. Requires the APRICOT_LOG_INFO env to be set to actually log
-  if (process.env[INFO_LOG_ENV] === 'true') {
+  if (process.env[INFO_LOG_ENV] !== 'false') {
     console.log(TAG_INFO + content);
   }
 }
